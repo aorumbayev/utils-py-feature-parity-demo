8d643288eb69d81d447d4f7a20e4e3090c880e5e
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2024-12-21 16:52:47 +0100

    chore: bump to latest

--- new.py
+++ new.py
@@ -5,6 +5,12 @@ from algokit_utils.applications.app_client import AppClientMethodCallWithSendPar
 from algokit_utils.models.amount import AlgoAmount
 from algokit_utils.transactions.transaction_composer import PaymentParams
 
+hello_world_arc32_app_spec = ApplicationSpecification.from_json(
+    (
+        Path(__file__).parent / "artifacts" / "testing_app" / "arc32_app_spec.json"
+    ).read_text()
+)
+
 # Gathering clients
 algorand = AlgorandClient.default_local_net()
 default_account = algorand.account.localnet_dispenser()
@@ -17,59 +23,71 @@ payment_params = PaymentParams(
     note=b"Hello World",
 )
 
-payment_response = algorand.send.payment(PaymentParams(
-                                            sender=default_account.address,
-                                            receiver=default_account.address,
-                                            amount=AlgoAmount.from_algo(1),
-                                            note=b"Hello World",
-                                        ))
+payment_response = algorand.send.payment(
+    PaymentParams(
+        sender=default_account.address,
+        receiver=default_account.address,
+        amount=AlgoAmount.from_algo(1),
+        note=b"Hello World",
+    )
+)
 print(payment_response)
 
 # Transaction groups
-pay_params_1 = PaymentParams(sender=default_account.address,
-                             receiver=default_account.address,
-                             amount=AlgoAmount.from_algo(1),
-                             note=b"Hello World1")
-pay_params_2 = PaymentParams(sender=default_account.address,
-                             receiver=default_account.address,
-                             amount=AlgoAmount.from_algo(1),
-                             note=b"Hello World2")
-response = algorand.new_group().add_payment(pay_params_1).add_payment(pay_params_2).send()
+response = (
+    algorand.new_group()
+    .add_payment(
+        PaymentParams(
+            sender=default_account.address,
+            receiver=default_account.address,
+            amount=AlgoAmount.from_algo(1),
+            note=b"Hello World1",
+        )
+    )
+    .add_payment(
+        PaymentParams(
+            sender=default_account.address,
+            receiver=default_account.address,
+            amount=AlgoAmount.from_algo(1),
+            note=b"Hello World2",
+        )
+    )
+    .send()
+)
 txn_response_1 = response.transactions[0]
 assert txn_response_1.payment
 # payment property is coming from TransactionWrapper that mimics behaviour of js-algorand-sdk v3 Transaction object
-assert txn_response_1.payment.amt == pay_params_1.amount
+assert txn_response_1.payment.amt == AlgoAmount.from_algo(
+    1
+)  # AlgoAmount can be compared to int
 
-# Raw App Client interactions
-random_account = algorand.account.random()
-dispenser_account = algorand.account.localnet_dispenser()
-algorand.account.ensure_funded(
-    account_to_fund=random_account.address,
-    dispenser_account=dispenser_account.address,
-    min_spending_balance=AlgoAmount.from_algo(10),
-    min_funding_increment=AlgoAmount.from_algo(1),
+## New App Client interactions
+### When passing an app spec to app factory, arc32 specs are auto converted ot arc56
+factory = algorand.client.get_app_factory(
+    app_spec=hello_world_arc32_app_spec, default_sender=default_account.address
 )
 
-## Manually creating an application
-raw_json_spec = Path(__file__).parent / "artifacts" / "testing_app" / "arc32_app_spec.json"
-hello_world_arc32_app_spec = ApplicationSpecification.from_json(raw_json_spec.read_text())
-
-# When passing an app spec to app factory, arc32 specs are auto converted ot arc56
-factory = algorand.client.get_app_factory(app_spec=hello_world_arc32_app_spec, default_sender=default_account.address)
-
-# Response is a tuple of app client and deploy result
-app_client, deploy_result = factory.deploy(
-    deploy_time_params={
-        "VALUE": 1,
-    },
-    deletable=True,
-    populate_app_call_resources=False,
+## Response is a tuple of app client and deploy result
+app_client, deploy_response = factory.deploy(
+    deploy_time_params={"VALUE": 1}, deletable=True, updatable=False
 )
+## Now you can interact with the app via the app client instance
+call = app_client.send.call(
+    AppClientMethodCallWithSendParams(method="call_abi", args=["test"])
+)
+assert call.abi_return.value == "Hello, test"
 
-print(deploy_result)
+## Metadata is accessible via `app` attribute
+assert deploy_response.app.app_id == app_client.app_id
+## You can infer operation perfomed via `operation_performed` attribute
+print(deploy_response.operation_performed)
+## Response exposes corresponding return type for each operation
+print(
+    deploy_response.create_response
+)  # Set when create or replace operation is performed
+print(deploy_response.delete_response)  # Set when replace operation is performed
+print(deploy_response.update_response)  # Set when update operation is performed
 
-# Now you can interact with the app via the app client instance
-call = app_client.send.call(AppClientMethodCallWithSendParams(method="call_abi", args=["test"]))
-
-assert call.return_value
-assert call.return_value.return_value == "Hello, test"
+## Accessing state
+assert app_client.state.global_state.get_all()["value"] == 1
+assert app_client.state.global_state.get_value("value") == 1
--- old.py
+++ old.py
@@ -1,18 +1,30 @@
+from pathlib import Path
+
 import algosdk
-from algokit_utils import get_algod_client, get_default_localnet_config, get_localnet_default_account
-from algosdk.atomic_transaction_composer import AtomicTransactionComposer, TransactionWithSigner
+from algokit_utils import (
+    ApplicationClient,
+    get_algod_client,
+    get_default_localnet_config,
+    get_indexer_client,
+    get_localnet_default_account,
+)
+from algosdk.atomic_transaction_composer import (
+    AtomicTransactionComposer,
+    TransactionWithSigner,
+)
 
 # Gathering clients
-algod_client_config = get_default_localnet_config()
+algod_client_config = get_default_localnet_config("algod")
 algod_client = get_algod_client(algod_client_config)
+indexer_client_config = get_default_localnet_config("indexer")
+indexer_client = get_indexer_client(indexer_client_config)
 default_account = get_localnet_default_account(algod_client)
 
 # Creating transactions
 sp = algod_client.suggested_params()
-payment = algosdk.transaction.PaymentTxn(sender=default_account.address,
-                                         receiver=default_account.address,
-                                         amt=1000000,
-                                         sp=sp)
+payment = algosdk.transaction.PaymentTxn(
+    sender=default_account.address, receiver=default_account.address, amt=1000000, sp=sp
+)
 signed_payment = payment.sign(default_account.private_key)
 
 # Sending transactions
@@ -21,10 +33,58 @@ print(f"Transaction ID: {tx_id}")
 
 # Transaction groups
 atc = AtomicTransactionComposer()
-g_pay_1 = algosdk.transaction.PaymentTxn(sender=default_account.address, receiver=default_account.address, amt=10000, sp=sp, note=b"Hello1")
-g_pay_2 = algosdk.transaction.PaymentTxn(sender=default_account.address, receiver=default_account.address, amt=10000, sp=sp, note=b"Hello2")
+g_pay_1 = algosdk.transaction.PaymentTxn(
+    sender=default_account.address,
+    receiver=default_account.address,
+    amt=10000,
+    sp=sp,
+    note=b"Hello1",
+)
+g_pay_2 = algosdk.transaction.PaymentTxn(
+    sender=default_account.address,
+    receiver=default_account.address,
+    amt=10000,
+    sp=sp,
+    note=b"Hello2",
+)
 atc.add_transaction(TransactionWithSigner(g_pay_1, default_account.signer))
 atc.add_transaction(TransactionWithSigner(g_pay_2, default_account.signer))
 
 response = atc.submit(algod_client)
 print(response)
+
+# Old App Client interactions
+app_client = ApplicationClient(
+    algod_client,
+    Path(__file__).parent / "artifacts" / "testing_app" / "arc32_app_spec.json",
+    creator=default_account,
+    indexer_client=indexer_client,
+)
+deploy_response = app_client.deploy(
+    signer=default_account.signer,
+    sender=default_account.address,
+    template_values={"VALUE": 1},
+    allow_delete=True,
+    allow_update=False,
+)
+
+## Metadata is accessible via `app` attribute
+assert deploy_response.app.app_id == app_client.app_id
+## You can infer operation perfomed via `operation_performed` attribute
+print(deploy_response.action_taken)
+## Response exposes corresponding return type for each operation
+print(
+    deploy_response.create_response
+)  # Set when create or replace operation is performed
+print(deploy_response.delete_response)  # Set when replace operation is performed
+print(deploy_response.update_response)  # Set when update operation is performed
+
+## Metadata is accessible via `app` attribute
+assert deploy_response.app.app_id == app_client.app_id
+
+## Now you can interact with the app via the app client instance
+call = app_client.call("call_abi", value="test")
+assert call.return_value == "Hello, test"
+
+## Accessing state
+assert app_client.get_global_state()["value"] == 1
